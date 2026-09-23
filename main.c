16fc81b998b19e0ffeb8c09e8dbe0eab75e33902
terehovae948-ship-it <terehovae948@gmail.com>
2026-09-23 20:07:58 +0600

    пытаюсь настроить показ инвентаря

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 
 #include <stdio.h>
 int current_day = 1;
-int current_nour = 8;
-static inventory[10] //4 мотыга 5 лейка 6 топор 7 корзина 8 сено 9 лопата
+int current_hour = 8;
+static inventory[10] //0 ничего 1 дерево 2 камень 3 семена 4 мотыга 5 лейка 6 топор 7 корзина 8 сено 9 лопата
 int work
+char predmet = ""
 int main() 
 { 
 	int i = 1;
@@ -22,8 +23,43 @@ int main()
 			printf("щас поработаем...");
 			printf("сколько изволите работать?");
 			scanf(" %d", &work);
+			current_hour += work
+				if (current_hour >= 24)
+				{
+					current_day += 1;
+					current_hour -= 24;
+
+				}
+			printf("Вы успешно поработали %d часов! Сейчас %d день, %d часов.", work, current_day, current_hour);
 		case 3:
-			printf("3");
+			printf("Вот ваш инвентарь:");
+			for (int a = 0; a < 10; a++)
+			{
+				switch (inventory[a])
+				{
+				case 0:
+					predmet = "Ничего"
+				case 1:
+					predmet = "Дерево"
+				case 2:
+					predmet = "Камень"
+				case 3:
+					predmet = "Семена"
+				case 4:
+					predmet = "Мотыга"
+				case 5:
+					predmet = "Лейка"
+				case 6:
+					predmet = "Топор"
+				case 7:
+					predmet = "Коззина"
+				case 8:
+					predmet = "Сено"
+				case 9:
+					predmet = "Лопата"
+				}
+				printf("%d Слот - %c", a + 1, predmet);
+			}
 		case 4:
 			printf("4");
 		case 5:
@@ -32,4 +68,5 @@ int main()
 			printf("6")
 		}
 	}
+return 0;
 }
